735e7bd87529a7743567cfd9470cff615f22b3fb
Tony Mogoa <mogoa.tonny@gmail.com>
2021-03-31 22:37:59 +0300

    Added login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+</head>
+<body class="w-100 h-100" style="background: url({{asset('img/bg.jpg')}});background-size: cover;background-repeat: no-repeat;background-attachment: fixed;">
+    <div class="d-flex flex-column justify-content-center align-items-center w-100 h-100">
+        <div class="d-flex flex-column justify-content-center align-items-center border rounded p-5 card shadow">
+            <img src="{{ asset('img/logo.svg') }}" alt="Chatlify" width="90px">
+            <h2 class="logo-text text-primary mt-2">Chatlify</h2>
+            <p>Chat your day away</p>
+            <form class="mt-1">
+                <div class="mb-3">
+                  <label for="username" class="form-label grey-font">Email address or username</label>
+                  <input type="username" class="border w-100 rounded-pill px-3 py-2" id="username" name="username" aria-describedby="usernameHelp" placeholder="Username or email">
+                  <div id="usernameHelp" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label grey-font">Password</label>
+                  <input type="password" class="border w-100 rounded-pill px-3 py-2" id="password" placeholder="Your password">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Log in</button>
+              </form>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,15 +12,17 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
-<body class="w-100 h-100">
+<body class="w-100 h-100" style="background: url({{asset('img/bg.jpg')}});background-size: cover;background-repeat: no-repeat;background-attachment: fixed;">
     <div class="d-flex justify-content-center align-items-center flex-column w-100 h-100">
         <div class="d-flex justify-content-center align-items-center flex-column border rounded p-5 card">
             <img src="{{ asset('img/logo.svg') }}" alt="Chatlify" width="150px">
-            <br/>
             <h2 class="logo-text text-primary mt-2">Chatlify</h2>
             <p>Chat your day away</p>
-            <button class="btn btn-primary btn-block">Start chatting away</button>
+            <a href="/login" class="btn btn-primary btn-block">Start chatting away</a>
             <span class="small-text mt-4">Built to ridicule the social media mess</span>
+        </div>
+        <div class="text-white d-flex flex-row justify-content-end w-100">
+            <span class="mr-3 mt-5">Photo by <a href="https://unsplash.com/@cristina_gottardi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="text-white">Cristina Gottardi</a> on <a href="https://unsplash.com/s/photos/chat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="text-white">Unsplash</a><span/>
         </div>
     </div>
 </body>
