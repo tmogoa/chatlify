e9ac928b86a0cc12e192911080c5fef718bf7011
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-07 21:01:57 +0300

    Really minor update

--- resources/views/chat/empty.blade.php
+++ resources/views/chat/empty.blade.php
@@ -4,5 +4,5 @@
     <p style="font-size: 17px;">Chat your day away</p>
     <div class="px-5 small-text" style="width: 600px">Our day to day life is, we must admit, very complicated nowadays. And who is to blame for that? We think it is the social media. Ask yourself this question?<br/>
         <div class="text-center font-weight-bold w-100">Can I spend a whole hour without checking on my phone?</div>Well is it a sad no?</div>
-    <div class="small-text font-weight-bold">What are you waiting for? Chat your day way. Waste your time!</div>
+    <div class="small-text font-weight-bold">So what are you waiting for? Chat your day way. Waste your time !</div>
 </div>
