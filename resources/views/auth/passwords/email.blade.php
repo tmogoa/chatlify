1ee36d22431dbfc983eb34ba0c0e8c2779263397
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-08 13:37:29 +0300

    Ui improvements

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="d-flex flex-column justify-content-center align-items-center w-100 h-100 py-4 container-wrap">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="w-100 d-flex flex-column align-items-center p-4">
+                    <img src="{{ asset('img/logo.svg') }}" alt="Chatlify" width="90px">
+                    <h2 class="logo-text text-primary mt-2">CHATLIFY</h2>
+                    <p>Chat your day away</p>
                 </div>
+                <div class="text-center mb-3 grey-font">Reset your password</div>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+        
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+        
+                    <div class="mb-3">
+                        <label for="email" class="form-label grey-font">{{ __('E-Mail Address') }}</label>
+                        <input id="email" type="email" class="border w-100 rounded-pill px-3 py-2 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+        
+                    <button type="submit" class="btn btn-primary btn-block mb-3">
+                        {{ __('Send Password Reset Link') }}
+                    </button>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,9 @@
             <div class="col-4 bg-white p-1 m-0">
                 <div class="d-flex flex-column">
                     <div class="d-flex flex-row align-items-center my-2 mx-3">
-                        <img src="{{ asset('img/logo.svg') }}" alt="Chatlify" width="45px"/>
+                        <a href="{{ route('home') }}">
+                            <img src="{{ asset('img/logo.svg') }}" alt="Chatlify" width="45px"/>
+                        </a>
                         <input type="text" class="border w-100 rounded-pill px-3 py-2 ml-2" id="search" name="search"
                             aria-describedby="searchHelp" placeholder="Search people"/>
                     </div>
